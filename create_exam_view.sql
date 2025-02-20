df3c717a07088a7e3578f1405804c3c7b6845f07
Mark S. Brown <sandcalc@gmail.com>
2025-02-20 17:11:26 +0000

    removed exam view nonsense

--- create_exam_view.sql
+++
@@ -1,8 +0,0 @@
-create view if not exists exam(id, date, part, examboard, qualification)
-as SELECT id, 
-json(metadata) -> '$.date' as date, 
-json(metadata) -> '$.part' as part, 
-json(metadata) -> '$.exam-board' as examboard,
-json(metadata) -> '$.level' as qualification 
-from pdfs
-order by date desc;
